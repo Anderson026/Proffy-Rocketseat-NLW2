b039e7739429df9965e19160484f769c12269c08
Anderson026 <anderson-ferreira22@hotmail.com>
2020-08-06 23:29:24 -0300

    Criando a estrutura de dados da página study, refatorando as rotas e configurando a view engine nunjucks

--- src/server.js
+++ src/server.js
@@ -1,20 +1,57 @@
+const proffys = [
+    { name: "Diego Fernandes",
+      avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+      whatsapp: "82996609725",
+      bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+      subject: "Química",
+      cost: "20", 
+      weekday: [0], 
+      time_from: [720], 
+      time_to: [1220]
+    },
+    { name: "Daniele Evangelista",
+      avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+      whatsapp: "82996609725",
+      bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+      subject: "Química",
+      cost: "20", 
+      weekday: [0], 
+      time_from: [720], 
+      time_to: [1220]
+    }
+]
+/* função para pegar o html e retornar na rota get */
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+/* pegando os dados da página study e retornar na segunda rota ger */
+function pageStudy(req, res) {
+    return res.render("study.html")
+}
+
+function pageGiveClasses(req, res) {
+    return res.render("give-classes.html")
+}
+
 const express = require('express')/* importando o express */
 
 const server = express()/* colocando o express na variável server */
-
-server.use(express.static("public"))/* importando os arquivos de Css */
-
-server.get("/", (req, res) => {/* primeira rota do tipo get */
-    return res.sendFile(__dirname + "/views/index.html")/* retornando o Html */
+/* importando o nunjucks */
+const nunjucks = require('nunjucks')
+/* configurar o nunjucks */
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,/* não guardar os arquivos no cache */
 })
 
-server.get("/study", (req, res) => {/* segunda rota do tipo get para pegar os dados */
-    return res.sendFile(__dirname + "/views/study.html")
-})
+/* configurar arquivos estáticos (css, scripts, imagens) */
+server.use(express.static("public"))
+/* rotas da aplicação */
+server.get("/", pageLanding)
 
-server.get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html")
-})
+server.get("/study", pageStudy)
+
+server.get("/give-classes", pageGiveClasses)
 
 server.listen(5500, () => {/* configurando a porta de comunicação */
     console.log('Servidor rodando na porta 5500');
