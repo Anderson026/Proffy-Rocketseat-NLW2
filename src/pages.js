4067c5b925a90c7a5feae9a2ce17b1314b98935a
Anderson026 <anderson-ferreira22@hotmail.com>
2020-08-08 18:33:58 -0300

    Criando a primeira busca na page study e adicionando uma validação no caso onde não retornar a busca mostrar uma mensagem para o usuário

--- src/pages.js
+++ src/pages.js
@@ -1,14 +1,50 @@
+/* importando o banco de dados */
+const Database = require('./database/db')
+
 /* desestruturando e importando os objetos de format.js */
-const { subjects, weekdays, getSubject } = require('./utils/format')
+const { subjects, weekdays, getSubject, convertHoursToMinutes } = require('./utils/format')
 
 /* função para pegar o html e retornar na rota get */
 function pageLanding(req, res) {
     return res.render("index.html")
 }
 /* pegando os dados da página study e retornar na segunda rota ger */
-function pageStudy(req, res) {
+async function pageStudy(req, res) {
     const filters = req.query/* variável para retornar os dados para o frontend */
-    return res.render("study.html", { proffys, filters, subjects, weekdays })
+
+    if (!filters.subject || !filters.weekday || !filters.time) {
+        return res.render("study.html", { filters, subjects, weekdays })
+    }
+    /* converter horas em minutos */
+    const timeToMinutes = convertHoursToMinutes(filters.time)
+    
+
+    const query = `
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE EXISTS(
+            SELECT class_schedule.*
+            FROM class_schedule
+            WHERE class_schedule.class_id = classes.id
+            AND class_schedule.weekday = ${filters.weekday}
+            AND class_schedule.time_from <= ${timeToMinutes}
+            AND class_schedule.time_to > ${timeToMinutes}
+        )
+        AND classes.subject = ${filters.subject}
+    `
+    /* caso haja erro na hora da consulta do banco de dados */
+    try {
+        
+        const db = await Database
+        const proffys = await db.all(query) 
+        
+        return res.render('study.html', { proffys, subjects, filters, weekdays })
+
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
 function pageGiveClasses(req, res) {
