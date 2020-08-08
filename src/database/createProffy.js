e72ba3972fc7a9fbbc023299ce2e7a33f86e3bdc
Anderson026 <anderson-ferreira22@hotmail.com>
2020-08-08 07:02:42 -0300

    Iniciando o dia 5 e criando o banco de dados sqlite para persistir os dados e para efetuar consultas

---
+++ src/database/createProffy.js
@@ -0,0 +1,19 @@
+/* função que vai exportar os dados do banco */
+module.exports = async function(db, {proffyValue, classValue, classScheduleValue}) {
+    /* inserir dados na tabela de proffys */
+    const insertedProffy = await db.run(`
+        INSERT INTO proffys (
+            name,
+            avatar,
+            whatsapp,
+            bio
+        ) VALUES (
+            ${proffyValue.name},
+            ${proffyValue.avatar},
+            ${proffyValue.whatsapp},
+            ${proffyValue.bio}
+        );
+    `)
+
+    const proffy_id = insertedProffy.lastID
+}
